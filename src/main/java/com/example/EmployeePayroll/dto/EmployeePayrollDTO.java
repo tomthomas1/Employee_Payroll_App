e939f5a8890f2c6558810d8891a45f2bd060cdd0
tomthomas1 <tomthomas8688@gmail.com>
2022-05-10 14:58:26 +0530

    S5-UC1 : Added remaining properties for the DTO and model

--- src/main/java/com/example/EmployeePayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/EmployeePayroll/dto/EmployeePayrollDTO.java
@@ -1,16 +1,20 @@
 package com.example.EmployeePayroll.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
 
     @NotNull(message = "ERROR: Name cannot be null!")
     @NotEmpty(message = "ERROR: Name cannot be empty!")
@@ -20,5 +24,17 @@ public class EmployeePayrollDTO {
     @NotNull(message = "ERROR: Salary cannot be null!")
     @Min(value = 5000, message = "ERROR: Please enter a salary greater than 5000")
     public long salary;
+
+    public String gender;
+
+    @JsonFormat(pattern = "dd MM yyyy")
+    public LocalDate startDate;
+
+    public String note;
+
+    public String profilePic;
+
+    public List<String> departments;
+
 
 }
--- src/main/java/com/example/EmployeePayroll/model/EmployeePayrollData.java
+++ src/main/java/com/example/EmployeePayroll/model/EmployeePayrollData.java
@@ -7,21 +7,34 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.List;
 
-@Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmployeePayrollData {
 
-    @Id
     private int employeeId;
     private String name;
-    private long salary;
+    private long salary;private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 
     public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO){
         this.employeeId=empId;
-        this.name=employeePayrollDTO.name;
-        this.salary=employeePayrollDTO.salary;
+        this.updateEmployeePayrollDTO(employeePayrollDTO);
     }
 
+    private void updateEmployeePayrollDTO(EmployeePayrollDTO employeePayrollDTO) {
+        this.name = employeePayrollDTO.name;
+        this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.startDate = employeePayrollDTO.startDate;
+        this.note = employeePayrollDTO.note;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.departments;
+    }
 }
