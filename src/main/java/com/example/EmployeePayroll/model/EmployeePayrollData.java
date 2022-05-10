668f216660318c39634f342292d7ebb69b699293
tomthomas1 <tomthomas8688@gmail.com>
2022-05-11 01:07:24 +0530

    S5-UC3 : Saving the employee payroll data to MySql DB

--- src/main/java/com/example/EmployeePayroll/model/EmployeePayrollData.java
+++ src/main/java/com/example/EmployeePayroll/model/EmployeePayrollData.java
@@ -5,22 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class EmployeePayrollData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int employeeId;
     private String name;
-    private long salary;private String gender;
+    private long salary;
+    private String gender;
+
+    @Column(name = "start_date")
     private LocalDate startDate;
     private String note;
+
+    @Column(name = "profile_pic")
     private String profilePic;
+
+    @ElementCollection
+    @CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
     private List<String> departments;
 
     public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO){
